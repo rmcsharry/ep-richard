ce14e909e1e2d87880d14d35559474fbb4e2ed5d
Basil Safwat <bsafwat@gmail.com>
2014-09-02 16:38:57 +0100

    Test that pod admins only see parents in their pod

--- spec/integration/pod_admin/pod_admin_parents_spec.rb
+++ spec/integration/pod_admin/pod_admin_parents_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe "Pod admin parents functionality", :js => false, :type => :featur
   before { login_as_pod_admin }
 
   describe "adding a parent" do
+    let!(:pod) { Fabricate(:pod) }
+    let!(:pod_admin) { Fabricate(:pod_admin, email: 'test@example.com', pod: pod ) }
+    let!(:parent) { Fabricate(:parent, name: 'Jen', pod: pod ) }
+
     it "should add the parent" do
       visit pod_admin_path
       click_link 'Add new parent'
@@ -12,16 +16,21 @@ RSpec.describe "Pod admin parents functionality", :js => false, :type => :featur
       fill_in 'Phone', with: '07515333333'
       click_button 'Add parent'
 
-      expect(Parent.all.count).to eq(1)
       expect(current_path).to eq(pod_admin_path)
       expect(page).to have_content('Sam')
+      expect(page).not_to have_content('Jen')
     end
 
     it "shouldn't be possible to add two parents with the same phone number" do
       Fabricate(:parent, phone: '07515223234')
       expect { Fabricate(:parent, phone: '07515223234') }.to raise_error
     end
+  end
 
+  describe "editing a parent" do
+    it "should update the parent" do
+
+    end
   end
 
 end
