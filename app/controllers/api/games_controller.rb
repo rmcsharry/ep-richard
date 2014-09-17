01c45899bf5c4addb2e81f0cdba5d8da29e0ae77
Basil Safwat <bsafwat@gmail.com>
2014-09-17 10:29:01 +0100

    Refactor controller

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -3,18 +3,8 @@ module API
 
     def index
       games = Game.where("in_default_set = true")
-      if params[:parent]
-        if parent = Parent.find(params[:parent])
-          if pod = parent.pod
-            if pod.go_live_date
-              pod_go_live_date = Date.parse(parent.pod.go_live_date.to_s)
-              number_of_weeks_since_go_live = ((Date.today - pod_go_live_date)/7).to_i
-              non_default_games = Game.where("in_default_set = false").order("created_at ASC")
-              extra_games = non_default_games[0, number_of_weeks_since_go_live]
-              games = games + extra_games
-            end
-          end
-        end
+      if parent = Parent.find(params[:parent])
+        games = games + Game.getExtraGamesForParent(parent)
       end
       render json: games, status: 200
     end
