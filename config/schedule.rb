0ad797b579aa24276f33da27227613ae765210d7
Basil Safwat <bsafwat@gmail.com>
2014-09-18 17:21:15 +0100

    Change to every minute

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,10 @@
 set :output, "#{path}/log/cron.log"
 
-every 1.day, :at => '10:00 am' do
+# every 1.day, :at => '10:00 am' do
+#   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
+#   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
+# end
+
+every 1.minute do
   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
-  command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
 end
