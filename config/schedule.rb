af348bc68a22b6f6a28020fb6940bd1b32e69af9
Basil Safwat <bsafwat@gmail.com>
2014-09-19 12:38:32 +0100

    Update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,6 @@
 set :output, "#{path}/log/cron.log"
 
-# every 1.day, :at => '10:00 am' do
-#   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
-#   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
-# end
-
-every 1.minute do
+every 1.day, :at => '11:00 am' do
   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
+  command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
 end
