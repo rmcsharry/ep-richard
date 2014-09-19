4b549809bb181d0e2260d65487eedf9e60794feb
Basil Safwat <bsafwat@gmail.com>
2014-09-19 12:58:08 +0100

    Make it run now

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 set :output, "#{path}/log/cron.log"
 
-every 1.day, :at => '11:00 am' do
+# every 1.day, :at => '11:00 am' do
+every 1.minute do
   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
 end
