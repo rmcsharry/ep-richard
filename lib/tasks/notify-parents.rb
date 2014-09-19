e46e75ca30f7ee2da328f979299c4693e10d22bb
Basil Safwat <bsafwat@gmail.com>
2014-09-19 12:25:01 +0100

    Add some cron logging

--- lib/tasks/notify-parents.rb
+++ lib/tasks/notify-parents.rb
@@ -1,7 +1,21 @@
+puts "\n== Starting notify job #{Time.now}\n\n"
+
 live_pods = Pod.where("go_live_date IS NOT null")
 
 live_pods.each do |pod|
+
+  puts "  Pod ##{pod.id}: #{pod.name}\n\n"
+
   pod.parents.each do |parent|
-    parent.notify
+    if parent.notify
+      puts "  - NOTIFIED: #{parent.name} (#{parent.phone})"
+    else
+      puts "  - SKIPPED: #{parent.name} (#{parent.phone})"
+    end
   end
+
+  puts "\n"
+
 end
+
+puts "== Finished notify job #{Time.now}\n\n"
