2d6044e0d2be5e1efb222e5d1646f3ed8a877154
Basil Safwat <bsafwat@gmail.com>
2015-07-05 15:12:00 +0200

    Move the video to its own page

--- app/controllers/pod_admin/games_controller.rb
+++ app/controllers/pod_admin/games_controller.rb
@@ -1,8 +1,22 @@
+require 'redcarpet'
+
 class PodAdmin::GamesController < PodAdminController
 
   def index
     @default_games = Game.default
     @weekly_games = Game.non_default
+  end
+
+  def show
+    @game = Game.find(params[:id])
+
+    if @game.instructions
+      renderer = Redcarpet::Render::HTML.new(no_links: true, hard_wrap: true)
+      @instructions = Redcarpet::Markdown.new(renderer).render(@game.instructions).html_safe
+    else
+      @instructions = ""
+    end
+
   end
 
 end
