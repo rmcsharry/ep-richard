327fe252ef5fe45ee2900284210fce0a78b80a9f
Basil Safwat <bsafwat@gmail.com>
2016-01-04 15:07:26 +1000

    Tweak mailer

--- app/mailers/pod_admin_mailer.rb
+++ app/mailers/pod_admin_mailer.rb
@@ -2,8 +2,8 @@ class PodAdminMailer < ApplicationMailer
   def analytics_email(pod_admin)
     @pod_admin = pod_admin
     @pod = @pod_admin.pod
-    subject = '[TEST - only being sent internally] EasyPeasy weekly report'
+    subject = '[TEST] EasyPeasy weekly report'
     mail(to: 'bsafwat@gmail.com', subject: subject)
-    # mail(to: 'hello@easypeasyapp.com', subject: subject)
+    mail(to: 'hello@easypeasyapp.com', subject: subject)
   end
 end
