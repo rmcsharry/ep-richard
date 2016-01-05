a406bb60feab725c789ae9bbddee34956f28073e
Basil Safwat <bsafwat@gmail.com>
2016-01-05 11:44:18 +1000

    Add test for capitals in email address

--- spec/integration/pod_admin/pod_admin_login_spec.rb
+++ spec/integration/pod_admin/pod_admin_login_spec.rb
@@ -14,6 +14,14 @@ RSpec.describe "Pod admin", :js => false, :type => :feature do
       expect(page).to have_content('Signed in successfully')
     end
 
+    it "should let you log in with capitals in your email address" do
+      visit admin_login_path
+      fill_in 'Email', with: 'Bsafwat+1@gmail.com'
+      fill_in 'Password', with: pod_admin.password
+      click_button 'Sign in'
+      expect(page).to have_content('Signed in successfully')
+    end
+
     describe "if your pod was deleted" do
       let!(:pod_admin) { Fabricate(:pod_admin, email: 'bsafwat+1@gmail.com', password: 'Password1' ) }
 
