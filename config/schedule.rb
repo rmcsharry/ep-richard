076f835342b64f255e1542d06f38d1e15a4937a5
Basil Safwat <bsafwat@gmail.com>
2016-01-06 08:28:22 +1000

    Kick off SMS sending twice a day

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,3 +4,9 @@ every 1.day, :at => '3:00 pm' do
   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
   command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
 end
+
+# This is a hack until we implement a queue
+every 1.day, :at => '4:30 pm' do
+  command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-parents.rb"
+  command "cd #{path} && /usr/local/bin/bundle exec rails runner -e production lib/tasks/notify-basil.rb"
+end
