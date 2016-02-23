c447ebf277f2fe062bfb75a5e2a260dc5079a4ab
Richard McSharry <rmcsharry@gmail.com>
2016-02-23 02:25:58 +0000

    Add TODO to the parent serializer

--- app/serializers/parent_serializer.rb
+++ app/serializers/parent_serializer.rb
@@ -9,6 +9,8 @@ class ParentSerializer < ActiveModel::Serializer
     object.pod.latest_comment
   end
   
+  # TODO: The pod_latest_comment is an embedded object inside Parent, so it only contains ids for parent and game, hence we need to serialize these properties
+  # but is there a better way to do this? How can we get the client to retrieve these properites without it making another server request to get them?
   def pod_latest_comment_parent_name
     if !object.pod.latest_comment.nil?
       object.pod.latest_comment.parent.name
