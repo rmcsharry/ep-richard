774220dbbd67aeb20b11c7979781bde833aefaf3
Richard McSharry <rmcsharry@gmail.com>
2016-02-24 02:40:53 +0000

    Update the TODO comment to clarify issue

--- app/serializers/parent_serializer.rb
+++ app/serializers/parent_serializer.rb
@@ -9,8 +9,10 @@ class ParentSerializer < ActiveModel::Serializer
     object.pod.latest_comment
   end
   
-  # TODO: The pod_latest_comment is an embedded object inside Parent, so it only contains ids for parent and game, hence we need to serialize these properties
-  # but is there a better way to do this? How can we get the client to retrieve these properites without it making another server request to get them?
+  # TODO:
+  # The pod_latest_comment is an embedded object inside Parent, so it only contains ids for parent and game, hence we need to serialize these properties here.
+  # Maybe there is a better way to do this? When there are lots of comments and parents, there is no guarantee that the parent or comment is already loaded on the client, so
+  # we have to send these embedded at this point. Perhaps it is possible to send a custom embedded object instead?
   def pod_latest_comment_parent_name
     if !object.pod.latest_comment.nil?
       object.pod.latest_comment.parent.name
