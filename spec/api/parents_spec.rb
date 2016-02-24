7c4f609a49b1ccc690fe7c8e19410c28f65467a0
Richard McSharry <rmcsharry@gmail.com>
2016-02-24 02:48:37 +0000

    Add another api test

--- spec/api/parents_spec.rb
+++ spec/api/parents_spec.rb
@@ -4,6 +4,22 @@ RSpec.describe "the parents API", :type => :request do
 
   describe "parents dashboard" do
 
+    it "returns no latest comment when there are none" do
+      game = Game.create!(name: "Test game", video_url: "https://minified.wistia.com/medias/q8x0tmoya2")
+
+      pod1 = Pod.create!(name: "Pod 1")
+      
+      parent1 = Parent.create!(name: "Richard", phone: "07963250340", pod: pod1)
+      parent2 = Parent.create!(name: "Gerard", phone: "07963250340", pod: pod1)
+
+      get "api/parents/#{parent1.slug}"
+      
+      parent_response = json(response.body)[:parent]
+      pod_latest_comment = parent_response[:pod_latest_comment]
+
+      expect(pod_latest_comment.as_json).to eq nil
+    end
+
     it "returns the latest comment from a parent in this pod" do
       game = Game.create!(name: "Test game", video_url: "https://minified.wistia.com/medias/q8x0tmoya2")
 
