3caa5311a37dafe0be488896af64119f26e920c6
Richard McSharry <rmcsharry@gmail.com>
2016-04-04 11:01:43 +0100

    Add CSV parents file upload for admins

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,4 +4,14 @@ class AdminController < ApplicationController
   def index
   end
 
+  def parents_import
+    @pod = Pod.find(params[:pod_id])
+    if @pod.nil?
+      flash[:notice] = "Pod not found. Import failed."
+    else
+      count = Parent.import(params[:file], @pod.id)
+      flash[:notice] = "#{count} parents imported to #{@pod.name}"
+    end
+    redirect_to admin_pods_url
+  end
 end
