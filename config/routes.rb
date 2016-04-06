17e485acd6041afda6aceb9de241eca7b3161983
Richard McSharry <rmcsharry@gmail.com>
2016-04-06 08:02:08 +0100

    Route changes for confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,10 @@ Rails.application.routes.draw do
   root 'site#index'
 
   # Admins
-  devise_for :admins
+  devise_for :admin, :controllers => {confirmations: 'admin/confirmations'}
 
   devise_scope :admin do
+    patch "/confirm" => "admin/confirmations#confirm"
     get '/admin/logout' => 'devise/sessions#destroy'
     get '/admin/signup', to: 'devise/registrations#new', as: 'admin_signup'
     get '/admin/login', to: 'devise/sessions#new', as: 'admin_login'
@@ -33,7 +34,8 @@ Rails.application.routes.draw do
   match '/pod_admin/send_welcome_sms/:id', to: 'pod_admin/parents#send_welcome_sms', via: [:post]
 
   resources :comments
-
+  resources :pod_admin
+  
   namespace :pod_admin do
     get '/', to: :index
     get '/dashboard', to: :dashboard
