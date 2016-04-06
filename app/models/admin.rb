34c0ff2717e4aca2b1614f661813d66ad3e74257
Richard McSharry <rmcsharry@gmail.com>
2016-04-06 08:01:50 +0100

    Password checks for signing up with only email

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,17 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :registerable
+  
+  def password_required?
+    super if confirmed?
+  end
+
+  def password_match?
+    self.errors[:password] << "can't be blank" if password.blank?
+    self.errors[:password_confirmation] << "can't be blank" if password_confirmation.blank?
+    self.errors[:password_confirmation] << "does not match password" if password != password_confirmation
+    password == password_confirmation && !password.blank?
+  end
+
 end
