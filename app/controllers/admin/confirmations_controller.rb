3f56b2e09572065fcf13ce3309e44c8875fb0ee7
Richard McSharry <rmcsharry@gmail.com>
2016-04-12 10:49:01 +0100

    Remove use of encrypted tokens

--- app/controllers/admin/confirmations_controller.rb
+++ app/controllers/admin/confirmations_controller.rb
@@ -21,17 +21,14 @@ class Admin::ConfirmationsController < Devise::ConfirmationsController
     elsif params[resource_name].try(:[], :confirmation_token).present?
       @original_token = params[resource_name][:confirmation_token]
     end
-    
-    digested_token = Devise.token_generator.digest(self, :confirmation_token, @original_token)
-    self.resource = resource_class.find_or_initialize_with_error_by(:confirmation_token, digested_token)  
+    self.resource = resource_class.find_or_initialize_with_error_by(:confirmation_token, @original_token)  
     
     super if resource.nil? or resource.confirmed?
   end
 
   def confirm
     @original_token = params[resource_name].try(:[], :confirmation_token)
-    digested_token = Devise.token_generator.digest(self, :confirmation_token, @original_token)
-    self.resource = resource_class.find_or_initialize_with_error_by(:confirmation_token, digested_token)    
+    self.resource = resource_class.find_or_initialize_with_error_by(:confirmation_token, @original_token)    
     resource.assign_attributes(permitted_params) unless params[resource_name].nil?
 
     if resource.valid? && resource.password_match?
