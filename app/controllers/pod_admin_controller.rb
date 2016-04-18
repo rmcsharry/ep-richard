bcf1a57a636163a6599edb2052b7f87e3466f8fb
Richard McSharry <rmcsharry@gmail.com>
2016-04-18 11:54:08 +0100

    Fix nilClass errors

--- app/controllers/pod_admin_controller.rb
+++ app/controllers/pod_admin_controller.rb
@@ -10,7 +10,7 @@ class PodAdminController < ApplicationController
       flash.now[:info] = build_flash_comment(current_admin.pod.latest_comment)
       # TODO: this is temporary, update later to show dashboard
       # redirect_to pod_admin_dashboard_path
-    elsif current_admin.pod.nil?
+    elsif current_admin.pod.blank?
       # this pod admin has not finished creating their pod (ie they signed-up via other website and admin/confirmations/create)
       redirect_to pod_admin_signup_path(id: 'step01')
     end
@@ -37,13 +37,13 @@ class PodAdminController < ApplicationController
 
   def expired
     # If pod is active, prevent showing the expired page
-    redirect_to pod_admin_path if current_admin.pod.is_active?
+    redirect_to pod_admin_path if current_admin.pod && current_admin.pod.is_active?
   end
   
   private
  
     def is_trial_expired
-      redirect_to pod_admin_expired_path if !current_admin.pod.is_active?
+      redirect_to pod_admin_expired_path if current_admin.pod && !current_admin.pod.is_active?
     end
     
 end
