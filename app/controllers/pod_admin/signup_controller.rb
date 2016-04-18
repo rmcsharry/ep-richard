7f1d63c7c8565490bf46ff7ddae2cd41f98972b4
Richard McSharry <rmcsharry@gmail.com>
2016-04-18 12:20:04 +0100

    Simplify free trial calculation

--- app/controllers/pod_admin/signup_controller.rb
+++ app/controllers/pod_admin/signup_controller.rb
@@ -49,7 +49,7 @@ class PodAdmin::SignupController < PodAdminController
   private
     def finish_wizard_path
       @pod = Pod.find(session[:pod_id])
-      @pod.inactive_date = DateTime.now.beginning_of_day + 15.days # free trial ends at start of 15th day (ie. midnight of the 14th day)
+      @pod.inactive_date = Date.today + 14.days # free trial ends at start of 15th day (ie. midnight of the 14th day)
       @pod.save
       # Now the wizard is finished, send the sms to all the parents that were added
       if current_admin.pod.parents.count > 0
