42934dc632cda3b53952462bc758e102fbdd1e72
Esteban Simon <admin@moppu.com>
2016-04-21 13:35:20 +0200

    Remove white spaces

--- lib/tasks/notify-parents.rb
+++ lib/tasks/notify-parents.rb
@@ -4,13 +4,13 @@ live_pods = Pod.where("go_live_date IS NOT null")
 
 live_pods.each do |pod|
 
-  puts "  Pod ##{pod.id}: #{pod.name} \n\n"
+  puts "  Pod ##{pod.id}: #{pod.name}\n\n"
 
   pod.parents.each do |parent|
     if parent.notify
-      puts "  - SENT WELCOME SMS: #{parent.name} (#{parent.phone}) "
+      puts "  - SENT WELCOME SMS: #{parent.name} (#{parent.phone})"
     else
-      puts "  - SKIPPED WELCOME SMS: #{parent.name} (#{parent.phone}) "
+      puts "  - SKIPPED WELCOME SMS: #{parent.name} (#{parent.phone})"
     end
   
     puts "\n"
