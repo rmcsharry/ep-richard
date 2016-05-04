c4fbce676ed366211b7c17e37dac4b168d81b91c
Richard McSharry <Richard McSharry>
2016-05-04 15:37:08 +0200

    Rename initializer so its clearer

--- app/assets/javascripts/initializers/inject_store_to_component_add_comment.js
+++ app/assets/javascripts/initializers/inject_store_to_component_add_comment.js
@@ -1,6 +1,6 @@
 Ember.onLoad('Ember.Application', function(Application) {
   Application.initializer({
-    name: 'injectStoreIntoMyComponent',
+    name: 'injectStoreIntoAddComment',
     after: 'store',
     initialize: function(container, application) {
       application.inject('component:add-comment', 'store', 'store:application');
