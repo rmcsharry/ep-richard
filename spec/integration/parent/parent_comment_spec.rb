a06021d27310afe5db73c1e6d42a85f56ef419da
Richard McSharry <Richard McSharry>
2016-05-10 12:27:02 +0200

    Add tests for comments ordering

--- spec/integration/parent/parent_comment_spec.rb
+++ spec/integration/parent/parent_comment_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.describe "Comments", :js => true, :type => :feature do
   let!(:pod){ Fabricate(:pod) }
   
-  let!(:parent){ Fabricate(:parent, name: 'Basil Safwat', phone: '07515333333', pod: pod) }
+  let!(:parent1){ Fabricate(:parent, name: 'Basil Safwat', phone: '07515333333', pod: pod) }
   let!(:parent2){ Fabricate(:parent, name: 'Bob Smith', pod: pod) }
   
   let!(:game){ Fabricate(:game, in_default_set: true) }
-  let!(:comment){ Fabricate(:comment, body: 'Here is my comment', parent: parent, game: game) }
+  let!(:comment1){ Fabricate(:comment, body: 'A comment from parent 1', parent: parent1, game: game) }
   let!(:comment2){ Fabricate(:comment, body: 'A comment from parent 2', parent: parent2, game: game) }
 
   let!(:pod2){ Fabricate(:pod) }
@@ -15,18 +15,23 @@ RSpec.describe "Comments", :js => true, :type => :feature do
 
   before do
     # must trigger a visit by both parents to avoid intro screens showing
-    Fabricate(:parent_visit_log, parent: parent)
+    Fabricate(:parent_visit_log, parent: parent1)
     Fabricate(:parent_visit_log, parent: parent2)
-    visit "/#/#{parent.slug}/games/"
+    visit "/#/#{parent1.slug}/games/"
     sleep 0.02
     find(:css, '.gameItem').click
   end
 
   describe "accessing the comments page for a game" do
     it "should display the comments page for the game" do
-      expect(page).to have_content('Here is my comment')
+      expect(page).to have_content('A comment from parent 1')
       expect(page).to have_content('A comment from parent 2')
-      expect(page).to have_content('Bob S.')
+      expect(page).to have_content('Bob S.')\
+    end
+    
+    it "should dispaly the comments in the reverse order they were created" do
+      expect(page).to have_text(/A comment from parent 2 .+ A comment from parent 1/)
+      expect(page).not_to have_text(/A comment from parent 1 .+ A comment from parent 2/)
     end
   end
 
@@ -48,13 +53,13 @@ RSpec.describe "Comments", :js => true, :type => :feature do
 
   describe "viewing commments" do
     it "should show you comments only from other members of your pod" do
-      visit "/#/#{parent.slug}/game/#{game.id}/"
-      expect(page).to have_content('Here is my comment')
+      visit "/#/#{parent1.slug}/game/#{game.id}/"
+      expect(page).to have_content('A comment from parent 1')
 
       visit "/"
 
       visit "/#/#{parent_from_pod2.slug}/game/#{game.id}/"
-      expect(page).not_to have_content('Here is my comment')
+      expect(page).not_to have_content('A comment from parent 1')
     end
   end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,43 +15,45 @@ RSpec.describe Comment, :type => :model do
     end
   end
 
-  describe "comments_for_pod" do
-    it "returns only the comments made by people in the specified pod" do
-      game = Game.create!(name: "Test game", video_url: "https://minified.wistia.com/medias/q8x0tmoya2")
+  describe "comments" do
+    let!(:game)     { Fabricate(:game, name: "Test game", video_url: "https://minified.wistia.com/medias/q8x0tmoya2") }
+    
+    let!(:pod1)     { Fabricate(:pod, name: "Pod 1") }
+    let!(:parent1)  { Fabricate(:parent, name:"Basil", phone: "07515222222", pod: pod1) }
+    let!(:comment1) { Fabricate(:comment, body: "First comment", game: game, parent: parent1) }
+    
+    let!(:pod2)     { Fabricate(:pod, name: "Pod 2") }
+    let!(:parent2)  { Fabricate(:parent, name:"Sam", phone: "07515333333", pod: pod2) }
+    let!(:comment2) { Fabricate(:comment, body: "Second comment", game: game, parent: parent2) }
+    
+    context "game method comments_for_pod" do
+      it "returns only the comments made by people in the specified pod" do
+        comments_for_pod_1 = game.comments_for_pod(pod1.id)
+        comments_for_pod_2 = game.comments_for_pod(pod2.id)
 
-      pod1 = Pod.create!(name: "Pod 1")
-      parent1 = Parent.create!(name: "Basil", phone: "07515222222", pod: pod1)
+        expect(comments_for_pod_1.length).to eq(1)
+        expect(comments_for_pod_2.length).to eq(1)
 
-      pod2 = Pod.create!(name: "Pod 2")
-      parent2 = Parent.create!(name: "Sam", phone: "07515333333", pod: pod2)
+        expect(comments_for_pod_1).not_to include(comment2)
+        expect(comments_for_pod_2).not_to include(comment1)
+      end
+      
+      it "returns comments for the pod in reverse created order" do
+        parent3 = Parent.create!(name: "Richard", phone: "07433107777", pod: pod1)
+        comment3 = Comment.create!(body: "Third comment", game: game, parent: parent3)
+        
+        expect(game.comments_for_pod(pod1.id)).not_to include(comment2)
+        expect(game.comments_for_pod(pod1.id)).not_to eq([comment1,comment3])
+        expect(game.comments_for_pod(pod1.id)).to eq([comment3,comment1])
+      end
+    end
 
-      comment1 = Comment.create!(body: "Hello", game: game, parent: parent1)
-      comment2 = Comment.create!(body: "Hey",   game: game, parent: parent2)
-
-      comments_for_pod_1 = game.comments_for_pod(pod1.id)
-      comments_for_pod_2 = game.comments_for_pod(pod2.id)
-
-      expect(comments_for_pod_1.length).to eq(1)
-      expect(comments_for_pod_2.length).to eq(1)
-
-      expect(comments_for_pod_1).not_to include(comment2)
-      expect(comments_for_pod_2).not_to include(comment1)
+    context "deleting a parent" do
+      it "also deletes the parent's comments" do
+        expect(Comment.all.length).to eq(2)
+        parent1.destroy
+        expect(Comment.all.length).to eq(1)
+      end
     end
   end
-
-  describe "deleting a parent" do
-    it "deletes the parent's comments" do
-      game = Game.create!(name: "Test game", video_url: "https://minified.wistia.com/medias/q8x0tmoya2")
-      pod = Pod.create!(name: "Pod 1")
-      parent = Parent.create!(name: "Basil", phone: "07515222222", pod: pod)
-      parent2 = Parent.create!(name: "Sam", phone: "07515333333", pod: pod)
-      Comment.create!(body: "Hello", game: game, parent: parent)
-      Comment.create!(body: "Hey",   game: game, parent: parent2)
-
-      expect(Comment.all.length).to eq(2)
-      parent.destroy
-      expect(Comment.all.length).to eq(1)
-    end
-  end
-
 end
