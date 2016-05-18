a4204942cd8c641f3e0fa7ef170e4dcf4fe514b8
Richard McSharry <Richard McSharry>
2016-05-18 17:14:35 +0200

    Remove logic used for testing

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -26,7 +26,7 @@ class Parent < ActiveRecord::Base
   end
 
   def send_welcome_sms
-    if Rails.env == "production" || Rails.env != "staging"
+    if Rails.env == "production" || Rails.env == "staging"
       account_sid = 'AC38de11026e8717f75248f84136413f7d'
       auth_token = 'f82546484dc3dfc96989f5930a13e508'
 
