67e17d2f7e233765f2bb9f5d78fdd74b31e0b068
Esteban Simon <admin@moppu.com>
2017-01-26 18:32:19 +0100

    Restrict access to parents once the pod becomes inactive

--- app/serializers/parent_serializer.rb
+++ app/serializers/parent_serializer.rb
@@ -3,6 +3,7 @@ class ParentSerializer < ActiveModel::Serializer
              :name,
              :first_name,
              :pod_name,
+             :pod_active,
              :phone,
              :slug,
              :pod_latest_comment,
@@ -13,15 +14,21 @@ class ParentSerializer < ActiveModel::Serializer
   def first_visit
     object.parent_visit_logs.count == 0
   end
-  
+
   def pod_name
     object.pod.name
   end
-  
+
+  def pod_active
+    return false if object.pod.go_live_date.nil?
+    return true if object.pod.inactive_date.nil?
+    Date.parse(object.pod.inactive_date.strftime("%F")) >= Date.parse(Date.today.strftime("%F"))
+  end
+
   def pod_latest_comment
     object.pod.latest_comment
   end
-  
+
   # TODO:
   # The pod_latest_comment is an embedded object inside Parent, so it only contains ids for parent and game, hence we need to serialize these properties here.
   # Maybe there is a better way to do this? When there are lots of comments and parents, there is no guarantee that the parent or comment is already loaded on the client, so
@@ -31,7 +38,7 @@ class ParentSerializer < ActiveModel::Serializer
       object.pod.latest_comment.parent.name
     end
   end
-  
+
   def pod_latest_comment_game_name
     if !object.pod.latest_comment.nil?
       object.pod.latest_comment.game.name
