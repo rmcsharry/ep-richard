ad080b49d33c97e9a811de90a8253a558e01cf83
Esteban Simon <admin@moppu.com>
2017-03-24 16:40:25 +0100

    Adapt tests to new logic

--- spec/integration/pod_admin/pod_admin_login_spec.rb
+++ spec/integration/pod_admin/pod_admin_login_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Pod admin", :js => false, :type => :feature do
-  
+
   describe "logging in as a pod admin" do
 
     let!(:pod_admin) { Fabricate(:pod_admin, email: 'bsafwat+1@gmail.com', password: 'Password1' ) }
@@ -25,15 +25,15 @@ RSpec.describe "Pod admin", :js => false, :type => :feature do
     describe "if your pod was deleted" do
       let!(:pod_admin) { Fabricate(:pod_admin, email: 'bsafwat+1@gmail.com', password: 'Password1' ) }
 
-      it "should throw an error" do
+      it "should allow you to create a new pod" do
         Pod.last.destroy
 
         visit admin_login_path
         fill_in 'Email', with: pod_admin.email
         fill_in 'Password', with: pod_admin.password
         click_button 'Sign in'
-        
-        expect(page).to have_content("Hm. I can't find your pod. Please contact a member of EasyPeasy staff to sort this out. Sorry for the inconvenience.")
+
+        expect(page).to have_content("Create your EasyPeasy pod")
       end
 
     end
