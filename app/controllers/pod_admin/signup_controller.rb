d73a685e89aa805826f72f09c2195a44d5905776
Esteban Simon <admin@moppu.com>
2017-03-24 14:24:49 +0100

    Trial pod queries - https://trello.com/c/pbsvI0o1

--- app/controllers/pod_admin/signup_controller.rb
+++ app/controllers/pod_admin/signup_controller.rb
@@ -2,9 +2,9 @@ class PodAdmin::SignupController < PodAdminController
   include Wicked::Wizard
   steps :step01, :step02, :step03
   @@sms_results = {}
-    
+
   layout 'pod_admin/signup'
-  
+
   def show
     case step
       when :step01
@@ -19,12 +19,14 @@ class PodAdmin::SignupController < PodAdminController
     end
     render_wizard
   end
-  
+
   def update
     case step
       when :step01
         @pod = get_pod_from_session(step)
         @pod.attributes = pod_params
+        @pod.inactive_date = Date.today + 14.days # free trial ends at start of 15th day (ie. midnight of the 14th day)
+        @pod.set_go_live_date
         if @pod.valid?
           @pod.save!
           current_admin.pod = @pod
@@ -40,16 +42,16 @@ class PodAdmin::SignupController < PodAdminController
         end
         render_wizard @pod # saves the parent, triggers validations
       when :step03
-        @pod = get_pod_from_session(step)      
+        @pod = get_pod_from_session(step)
         if try_build_parents && !@pod.valid?
           @pod.parents.build
         end
         render_wizard @pod # saves the pod, triggers validations
     end
   end
-  
+
   private
-  
+
     def get_pod_from_session(step)
       if session[current_admin.id].nil? && step == :step01
         pod = Pod.new(pod_params)
@@ -59,21 +61,19 @@ class PodAdmin::SignupController < PodAdminController
       #TODO edge case: what to do if pod.nil?
       return pod
     end
-    
+
     def send_sms_to_first_parent(parent)
       return if parent.nil?
       @@sms_results = { parent.name => try_sending_welcome_sms(parent) }
     end
-    
+
     def try_sending_welcome_sms(parent)
       parent.send_welcome_sms
       return (parent.errors.count > 0)
     end
-    
+
     def finish_wizard_path
       @pod = Pod.find(session[current_admin.id])
-      @pod.inactive_date = Date.today + 14.days # free trial ends at start of 15th day (ie. midnight of the 14th day)
-      @pod.set_go_live_date
       # Now the wizard is finished, send the sms to the parents that were added (if any)
       if current_admin.pod.parents.count > 0
         try_to_send_sms_to_new_parents
@@ -81,13 +81,13 @@ class PodAdmin::SignupController < PodAdminController
       end
       pod_admin_path
     end
- 
+
     def try_to_send_sms_to_new_parents
       current_admin.pod.parents.where('welcome_sms_sent = ?', false).each do |parent|
         @@sms_results.store(parent.name, try_sending_welcome_sms(parent))
       end
     end
-    
+
     def flash_sms_results
       sms_errors = @@sms_results.select{|k,v| v == false}
       if sms_errors.count == 0
@@ -96,15 +96,15 @@ class PodAdmin::SignupController < PodAdminController
         flash[:danger] = "Whoops! Something went wrong - please try sending again using the Parents menu. The SMS did not get sent for: #{format_sms_results(false)}."
       end
     end
-    
+
     def format_sms_results(result)
       sms_errors = @@sms_results.map{|k,v| k if v == result}.compact
       sms_errors.map{|s| "#{s}"}.join(', ')
     end
-    
+
     def try_build_parents
       is_blank_submitted = false
-      pod_params[:parents_attributes].each do |k,v| 
+      pod_params[:parents_attributes].each do |k,v|
         if v[:name].blank? && v[:phone].blank?
           v.merge!({:_destroy => '1'}) # when the pod saves, this empty record will be discarded
           is_blank_submitted = true # since we discarded an empty record, we need to put it back in the view (only if validations fail on the other record)
@@ -114,7 +114,7 @@ class PodAdmin::SignupController < PodAdminController
       end
       return is_blank_submitted
     end
-    
+
     def pod_params
       params.require(:pod).permit(:id, :name, parents_attributes: [:id, :name, :phone])
     end
