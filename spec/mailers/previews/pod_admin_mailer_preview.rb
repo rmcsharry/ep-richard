a035f2e7f8c3200f163853b0f026d77c74a5d82e
rmcsharry <rmcsharry@gmail.com>
2017-04-07 15:18:49 +0000

    Changes preview to use latest podadmin

--- spec/mailers/previews/pod_admin_mailer_preview.rb
+++ spec/mailers/previews/pod_admin_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/
 class PodAdminMailerPreview < ActionMailer::Preview
   def preview
-    PodAdminMailer.analytics_email(Admin.where(id: 46).first)
+    PodAdminMailer.analytics_email(PodAdmin.last)
   end
 end
