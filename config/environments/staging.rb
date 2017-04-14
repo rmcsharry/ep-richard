3981e4d956b5ddd89920288722f3acd246722401
Richard McSharry <Richard McSharry>
2017-04-14 11:46:24 +0200

    Precompile assets to true for Heroku staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
